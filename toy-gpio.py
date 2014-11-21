e9982bffd8fdd54628a4543dc33afdd824f6aa64
Martin Oldfield <ex-git@mjoldfield.com>
2014-11-21 22:38:03 +0000

    Made blinky

--- toy-gpio.py
+++ toy-gpio.py
@@ -1,5 +1,16 @@
 import Adafruit_GPIO as GPIO
+import time
 
 gpio = GPIO.get_platform_gpio()
 
-print gpio
+pin = 338
+
+gpio.setup(pin, "out")
+
+while True:
+    gpio.output(pin, 1)
+    time.sleep(0.5)
+    gpio.output(pin, 0)
+    time.sleep(0.5)
+
+
